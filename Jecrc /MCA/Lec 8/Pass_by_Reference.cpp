61c72c13b240514e156564a5479e5729f91fecb9
Ariv-lamba <108919123+Ariv-lamba@users.noreply.github.com>
2025-04-23 12:05:43 +0530

    Create Pass_by_Reference.cpp

---
+++ Jecrc /MCA/Lec 8/Pass_by_Reference.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+void swap ( int &a, int &b){
+    int c = a;
+    a = b;
+    b = c;
+    cout << a<< " " <<b <<endl;
+}
+int main() {
+    int a = 10;
+    int b = 15;
+    swap(a, b);
+    cout << a << " "<<b<<endl;
+    return 0;
+}
