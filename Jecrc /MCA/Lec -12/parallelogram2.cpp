cd1b756f4eb56bd070853cee5b5e94fdd297ce23
Ariv-lamba <108919123+Ariv-lamba@users.noreply.github.com>
2025-05-05 18:41:02 +0530

    Create parallelogram2.cpp

---
+++ Jecrc /MCA/Lec -12/parallelogram2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    
+    for( int row = 1; row <=n ; row++){
+        if(row==1){
+            for(int i =0; i< n-1; i++){
+                cout << "a ";
+            }
+            for(int i =0; i<n; i++){
+                cout << "* ";
+            }
+            cout <<endl;
+        }
+         if(row==n){
+             for(int i =0; i<n; i++){
+                cout << "* ";
+            }
+            for(int i =0; i< n-1; i++){
+                cout << "a ";
+            }
+        }
+        if(row>1 && row<n){
+            for(int i =0; i< n-row; i++){
+                cout << "a ";
+            }
+           cout << "* ";
+           for(int i =0; i<n-2; i++){
+               cout << "a ";
+           }
+           cout << "* ";
+           
+           for(int i =0; i< row-1; i++){
+               cout << "b ";
+           }
+           
+           
+           cout << endl;
+        }
+    }
+    
+    return 0;
+}
