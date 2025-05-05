53295c0821769313fe8e160d1ce58ddb3422b301
Ariv-lamba <108919123+Ariv-lamba@users.noreply.github.com>
2025-05-05 18:32:32 +0530

    Create pattern2.cpp

---
+++ Jecrc /MCA/Lec -10/pattern2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+int n ;
+cin>>n;
+
+for( int row = 1; row <= n; row++){
+   // cout << " i am in row no - " << row  <<endl;
+    // stars;
+
+    for(int i =0; i<row; i++){
+        cout << "  ";
+    }
+    
+    
+    // space 
+    for(int j =0; j<n-(row); j++){
+        cout << "* ";
+    }
+    
+    cout << endl;
+}
+return 0;
+
+}
