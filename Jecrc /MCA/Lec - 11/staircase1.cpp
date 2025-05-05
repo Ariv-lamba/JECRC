65ac1dd315d72f86c19234c21b99e93f7c8e262a
Ariv-lamba <108919123+Ariv-lamba@users.noreply.github.com>
2025-05-05 18:39:02 +0530

    Create staircase1.cpp

---
+++ Jecrc /MCA/Lec - 11/staircase1.cpp
@@ -0,0 +1,27 @@
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+int n ;
+cin>>n;
+
+for(int row = 0; row<n; row++){
+    
+  // cout << " i am in row number -> " << row << endl;
+  int x = 1;
+  for(int i =0;  i< row+1; i++){
+      cout <<  x<< " ";
+      x++;
+  }
+    cout << endl;
+    
+    
+}
+
+
+
+return 0;
+
+}
