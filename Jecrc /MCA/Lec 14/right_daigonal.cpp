7b245d3bf58cc4f3731246af038bedef49e620df
Ariv-lamba <108919123+Ariv-lamba@users.noreply.github.com>
2025-05-09 14:57:16 +0530

    Create right_daigonal.cpp

---
+++ Jecrc /MCA/Lec 14/right_daigonal.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int m ;
+    cin>>m;
+    
+    for(int row = 0; row<n; row++){
+        for(int col = 0; col<m; col++){
+           if( row ==0 || row == n-1 || col == 0 || col == m-1 || row+col == n-1){
+                cout << "* ";
+            }
+            else{
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+   return 0;}
