68f20113d175dcb14b9b6886c5eaf9e524c056f6
Ariv-lamba <108919123+Ariv-lamba@users.noreply.github.com>
2025-05-09 12:57:29 +0530

    Create Pattern with Zeros.cpp

---
+++ Jecrc /MCA/Lec 16/Pattern with Zeros.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n;
+	cin>>n;
+	
+	for(int i =0; i<n; i++){
+		int x = i+1;
+		for(int j=0; j<i+1; j++){
+			if(j == 0 || j == i){
+				cout << x << " ";
+			}else{
+				cout << 0 << " ";
+			}
+			// x++;
+		}
+		// for(int j =0; j<n-1-i; j++){
+		// 	cout << "*";
+		// }
+		cout <<endl;
+	}
+	return 0;
+}
